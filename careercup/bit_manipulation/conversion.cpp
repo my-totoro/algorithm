3efd7e76949bf24b301362b2479ded5b15861934
twang2015@outlook.com <twang2015@outlook.com>
2016-07-11 01:26:46 +0800

    finish bit operation

--- careercup/bit_manipulation/conversion.cpp
+++ careercup/bit_manipulation/conversion.cpp
@@ -10,3 +10,33 @@
  * Output: 2
  *
  */
+
+#include<iostream>
+
+//perform XOR operation and then count the number of 1s
+int convertion(int val1, int val2)
+{
+	int count = 0;
+	for (int val = val1^val2; val != 0; val >>= 1){
+		if (val & 1) count++;
+	}
+	return count;
+}
+
+//val & (val-1) always clear the last 1, more efficient, remember this
+int convertion_optimal(int val1, int val2)
+{
+	int count = 0;
+	for (int val = val1^val2; val != 0; val = val & (val - 1)){
+		count++;
+	}
+	return count;
+}
+
+int main(int argc, char* argv[])
+{
+	int val1 = -10;
+	int val2 = -20;
+	std::cout << convertion_optimal(val1, val2) << '\t' << convertion(val1, val2) << '\n';
+	return 0;
+}
--- careercup/bit_manipulation/pairwise_swap.cpp
+++ careercup/bit_manipulation/pairwise_swap.cpp
@@ -3,3 +3,26 @@
  * possible (e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, and so on).
  *
  */
+
+
+/*
+Solution: shift left 1 bit and mask with 1010...1010 will get the odd bits with even ones
+shift right 1 bit and mask with 0101...0101 will get the even bits with odd ones
+combine them with OR operation.
+*/
+#include<iostream>
+
+
+int pair_swap(int val)
+{
+	int val1 = (val << 1) & 0xaaaaaaaa;
+	int val2 = (val >> 1) & 0x55555555;
+	return val1 | val2;
+}
+
+int main()
+{
+	int val = 13;
+	std::cout << pair_swap(val) << std::endl;
+	return 0;
+}
